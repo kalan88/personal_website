18fe6d969856dae972fcf8fa7651d91ed65b5e21
kalan <visdiver239@gmail.com>
2025-04-03 16:45:12 -0400

    created structure

---
+++ pweb/src/pages/About.js
@@ -0,0 +1,6 @@
+import '../css/About.css'
+
+const About = () => {
+    return <h1 className="about-title">About Us</h1>;
+  };
+  export default About;
---
+++ pweb/src/pages/Home.js
@@ -0,0 +1,6 @@
+import '../css/Home.css';
+
+const Home = () => {
+    return <h1>Welcome to the Home Page</h1>;
+  };
+  export default Home;
